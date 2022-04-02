7739e42491be3009a69f14e2b23219f91f80f6ee
hackprotech <78909147+hackprotech@users.noreply.github.com>
2022-04-02 23:01:05 +0530

    MainDemo with Examples

---
+++ src/basics/MainDemo.scala
@@ -0,0 +1,12 @@
+package basics
+
+object MainDemo {
+
+  def main(a: Array[String]): Unit = {
+    println("Hello HackPro Community!!")
+    println(a(0))
+    println(a(1))
+    println(a(2))
+  }
+
+}
